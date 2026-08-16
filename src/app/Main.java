6628fad95037940f2602a102d722a7080fab1ba6
Miguel Gómez Tobón <miguel.gomez35@eia.edu.co>
2026-08-15 19:02:24 -0500

    Se hace el main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,14 @@
 package app;
 
+import ui.MainFrame;
+
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
 }
